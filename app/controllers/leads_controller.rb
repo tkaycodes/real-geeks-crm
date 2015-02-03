81acc96bda3238d4b3c3eaed9ffb6254e2678d1b
LupineDev <chris@lupinedev.com>
2015-02-03 10:24:12 -1000

    Added leads list with dummy data

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,4 +1,7 @@
 class LeadsController < ApplicationController
   def index
+    @lead_class = Struct.new(:name, :start_date, :last_active_at, :last_search, :agent)
+    @leads = []
+    20.times { @leads << @lead_class.new("Frank Miller", 3.months.ago, 2.days.ago, "$50,000+ Honolulu, type: Condo", "Jeff Manson") }
   end
 end
