831ad47c64b143051b4a8debdd7f4f20494e5188
LupineDev <chris@lupinedev.com>
2015-02-04 10:54:08 -1000

    Lead detail page

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,5 +1,13 @@
 class LeadsController < ApplicationController
   def index
-    @leads = Lead.order "created_at DESC"
+    if  current_agent.role == "Admin"
+      @leads = Lead.order "created_at DESC"
+    else
+      @leads = current_agent.leads.order "created_at DESC"
+    end
+  end
+
+  def show
+    @lead = Lead.find(params[:id])
   end
 end
--- spec/controllers/leads_controller_spec.rb
+++ spec/controllers/leads_controller_spec.rb
@@ -38,6 +38,38 @@ RSpec.describe LeadsController, :type => :controller do
         expect(Lead).to receive(:order).with("created_at DESC")
         get :index
       end
+
+      it "should retrieve all the leads ordered by most recently active" do
+        get :index
+        assigns(:leads).size.should eq(Lead.count)
+      end
+    end
+    context "when signed in as an agent" do
+      before :each do
+        sign_in @agent
+      end
+
+      it "should retrieve all the leads ordered by most recently active" do
+        get :index
+        assigns(:leads).size.should eq(@agent.leads.size)
+        assigns(:leads).should include(@agent.leads.sample)
+      end
+    end
+  end
+
+  describe "GET#show" do
+    let(:lead) {Lead.create(
+      name: Faker::Name.name, email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number, start_date: 10.days.ago,
+      active_at: (1..10).to_a.sample.hours.ago, agent: [@agent, @admin].sample
+    )}
+    before :each do
+      sign_in @admin
+    end
+
+    it "should find the lead based on params[:id]" do
+      get :show, id: lead.to_param
+      assigns(:lead).should eq(lead)
     end
   end
 
