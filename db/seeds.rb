ec216fc5769aaa17579b45ca45c84d2bf9432740
LupineDev <chris@lupinedev.com>
2015-02-03 14:22:27 -1000

    Require login via devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating Users..."
+Agent.create(email: "jeff@realgeeks.com", password: "testtest", name: "Jeff Manson", role: "Admin", phone: "808-880-8881")
+Agent.create(email: "kevin@realgeeks.com", password: "testtest", name: "Kevin McCarthy", role: "Admin", phone: "808-880-8882")
+Agent.create(email: "chris@realgeeks.com", password: "testtest", name: "Chris Sass", role: "Agent", phone: "808-880-8883")
