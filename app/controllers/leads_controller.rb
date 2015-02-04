9209b999fd06fc3454db2d0aadc3969fcee49aac
LupineDev <chris@lupinedev.com>
2015-02-04 11:17:48 -1000

    Update existing lead

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,4 +1,5 @@
 class LeadsController < ApplicationController
+  before_filter :get_lead, only: [:show, :update]
   def index
     if  current_agent.role == "Admin"
       @leads = Lead.order "created_at DESC"
@@ -8,6 +9,24 @@ class LeadsController < ApplicationController
   end
 
   def show
+  end
+
+  def update
+    if @lead.update_attributes(lead_params)
+      redirect_to lead_path(@lead), notice: "Successfully updated Lead"
+    else
+      flash[:error] = "Could not save lead: #{@lead.errors.full_messages.join(', ')}"
+      render "show"
+    end
+  end
+
+  private
+
+  def lead_params
+    params[:lead].permit(:name, :email, :phone, :address, :agent_id)
+  end
+
+  def get_lead
     @lead = Lead.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :agents
   root 'leads#index'
 
-  resources :leads, only: [:index, :show]
+  resources :leads, only: [:index, :show, :update]
   resource :settings, only: [:show, :update]
 
 end
